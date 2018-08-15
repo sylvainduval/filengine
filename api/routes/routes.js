ba65dca11896863f56262e59fe1563f78302dd1b
Sylvain Duval <sylvain@vps532725.ovh.net>
2018-08-15 19:46:08 +0200

    Adding cross-origin requests

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -39,6 +39,9 @@ module.exports = function() {
 	api.use(bodyParser.json())
 
 	api.use(function (req, res, next) {
+		
+		res.header("Access-Control-Allow-Origin", "*");
+		res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 
 		if ( req.originalUrl != '/login' && req.originalUrl !='/register' && sess.checkAuth(req, res) == false) {
 
